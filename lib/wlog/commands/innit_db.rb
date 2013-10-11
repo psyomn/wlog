8652b3d7ff3e3dc64c207d5366e3e043c133910e
psyomn <lethaljellybean@gmail.com>
2013-10-11 02:12:56 -0400

    Minor changes

--- lib/wlog/commands/innit_db.rb
+++ lib/wlog/commands/innit_db.rb
@@ -9,9 +9,7 @@ module Wlog
 class InnitDb < Commandable
   include StaticConfigurations
   def execute
-    DbRegistry.instance
     current_dir = "#{File.expand_path File.dirname(__FILE__)}/../sql"
-    p current_dir
     turntable   = Turntables::Turntable.new
     turntable.register(current_dir)
     turntable.make_at!("#{DataDirectory}/#{ARGV[0] || DefaultDb}")
