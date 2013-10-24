4630d627b5b5f4f114e35991daea8684fca6ddd9
psyomn <lethaljellybean@gmail.com>
2013-10-23 22:46:27 -0400

    Add setup wizard.

---
+++ lib/wlog/ui/setup_wizard.rb
@@ -0,0 +1,24 @@
+require 'wlog/domain/sys_config'
+module Wlog
+# This is the ui that is displayed whenever we detect that it is the first
+# time that the application runs.
+# @author Simon Symeonidis
+class SetupWizard
+  def initialize(db)
+    @db = db
+    @sys = SysConfig.new(db)
+  end
+
+  # Call this to prompt the user for things
+  def run
+    input = ""
+    until ['yes', 'no'].include? input
+      puts "Do you use a terminal that supports ANSI colors? [yes/no] :"
+      input = $stdin.gets
+      input.chomp!
+    end
+    @sys.ansi! if input == 'yes'
+    @sys.not_ansi! if input == 'no'
+  end
+end
+end
