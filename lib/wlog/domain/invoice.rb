73ff8025aa08849507295b6ba869082a0b62fc04
psyomn <lethaljellybean@gmail.com>
2014-01-29 00:35:58 -0500

    Add invoice domain object

---
+++ lib/wlog/domain/invoice.rb
@@ -0,0 +1,9 @@
+module Wlog
+# The invoice domain object - use this to manipulate invoice recordings, or 
+# this along with some renderer and templates in order to create said invoices.
+# @author Simon Symeonidis
+class Invoice
+  def initialize
+  end
+end
+end
