9f178fe48ce3afb3fb3b1b5c4cd96cbd5fd883ce
psyomn <lethaljellybean@gmail.com>
2014-07-13 13:57:19 -0400

    invoice.rb: add stubs for now

--- lib/wlog/domain/invoice.rb
+++ lib/wlog/domain/invoice.rb
@@ -3,7 +3,16 @@ module Wlog
 # this along with some renderer and templates in order to create said invoices.
 # @author Simon Symeonidis
 class Invoice
-  def initialize
+  def initialize(date_from, date_to)
   end
+
+
+  attr_accessor :date_from
+
+  attr_accessor :date_to
+
+  # Ultimately this is the location where the invoice template is (in the
+  # $HOME/.config/wlog/templates directory).
+  attr_accessor :template_location
 end
 end
