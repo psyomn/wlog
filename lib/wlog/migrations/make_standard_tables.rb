5d29e830d7ec972fd4537d729832019079d2c475
psyomn <lethaljellybean@gmail.com>
2014-09-27 02:46:16 -0400

    migrations/standard: add the tables as migrations

---
+++ lib/wlog/migrations/make_standard_tables.rb
@@ -0,0 +1,48 @@
+require 'active_record' 
+
+
+module Wlog
+# Migrations to replace the raw sql from turntables
+# @author Simon Symeonidis
+class MakeStandardTables < ActiveRecord::Migration
+  def change 
+    create_table :issues do |t|
+      t.text :description
+      t.datetime :due_date
+      t.integer :status
+      t.bigint :timelog
+      t.text :long_description
+    end
+
+    create_table :log_entries do |t|
+      t.text :description
+      t.integer :issue_id
+    end
+
+    create_table :attachments do |t|
+      t.text :filename 
+      t.text :given_name
+      t.blob :data
+    end
+
+    create_table :key_values do |t|
+      t.text :key
+      t.text :value
+    end
+  end
+end
+end 
+
+
+=begin
+TODO
+-- A polymorphic relationship for attachments. So pretty much anything that
+-- wants to have something attached, uses the discriminator in order to 
+-- specify itself, as well as its id. 
+CREATE TABLE polymorphic_attachments (
+  discriminator    TEXT, 
+  discriminator_id INTEGER, 
+  attachment_id    INTEGER
+);
+
+=end
