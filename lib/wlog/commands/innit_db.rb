61a5f78decaf21e8d60ede9bc0196f2848131e5e
psyomn <lethaljellybean@gmail.com>
2014-09-27 02:27:00 -0400

    commands/innit: migrate to migrations

--- lib/wlog/commands/innit_db.rb
+++ lib/wlog/commands/innit_db.rb
@@ -4,21 +4,60 @@ require 'wlog/domain/static_configurations'
 require 'standalone_migrations'
 require 'rake'
 
+require 'active_record'
+require 'sqlite3'
+
 module Wlog
+
+# Only used here
+class MakeSchemaMigration < ActiveRecord::Migration
+  def change
+    create_table :schema_migrations do |t|
+      t.text :version
+    end
+  end
+end
+
+# Only used here
+class SchemaMigration < ActiveRecord::Base
+end
+
 # Command to create the initial database
 # @author Simon Symeonidis
 class InnitDb < Commandable
   include StaticConfigurations
   def execute
     current_dir = "#{File.expand_path File.dirname(__FILE__)}/../sql"
-    # turntable.make_at!("#{DataDirectory}#{ARGV[0] || DefaultDb}") 
-
-    # You need to set the env variale to false, else the migrations will pring
-    # on the command line
-    StandaloneMigrations::Tasks.load_tasks
-    ENV['VERBOSE'] = 'false'
-    Rake::Task["db:migrate"].invoke
+    make_schema_migrations!
+    execute_migrations!
   end
+
+private
+
+  # Checks to see if versioning table is there. Create if not.
+  def make_schema_migrations! 
+    ActiveRecord::Base.configurations = dbconfig
+    ActiveRecord::Base.establish_connection('development')
+
+    unless SchemaMigration.table_exists?
+      ActiveRecord::Migration.verbose = false 
+      ActiveRecord::Migration.run(MakeSchemaMigration)
+    end
+  end
+
+  def execute_migrations!
+  end
+
+  # TODO this should probably be moved
+  def dbconfig
+    dbname = ARGV[0] || DefaultDb
+    {'development' => {
+     :adapter => 'sqlite3',
+     :pool => 5,
+     :database => DataDirectory + dbname,
+     :timeout => 5000 }}
+  end
+
 end
 end
 
