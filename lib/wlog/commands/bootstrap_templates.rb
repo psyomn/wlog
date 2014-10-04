d4a70063a2b2f3a9b2222a0bff5db6c994e268e9
psyomn <lethaljellybean@gmail.com>
2014-10-04 17:57:46 -0400

    bootstrap_templates: update template to give more detailed example

--- lib/wlog/commands/bootstrap_templates.rb
+++ lib/wlog/commands/bootstrap_templates.rb
@@ -26,6 +26,11 @@ private
   def write_default_template!
     fh = File.open(TemplateSampleFile, 'w')
     data = "A list of issues:
+<p> Invoice: <%= @invoice.description %> </p>
+<p> From: <%= @invoice.from.strftime(\"%A %d %B \") %>  </p>
+<p> To: <%= @invoice.to.strftime(\"%A %d %B\") %> </p>
+<p> Work Items to report: <%= @log_entries.count %> </p>
+
 <% @issues.each do |issue| %>
   <%= \"\#{issue.id}  \#{issue.description}\" %>
 <% end %>"
