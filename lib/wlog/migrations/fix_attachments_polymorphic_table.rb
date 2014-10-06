028b1e512d9d2681a1fe96c72576b23f50f6ab60
psyomn <lethaljellybean@gmail.com>
2014-10-05 22:46:44 -0400

    migrations: add polymorphic alterations for attachment

---
+++ lib/wlog/migrations/fix_attachments_polymorphic_table.rb
@@ -0,0 +1,15 @@
+require 'active_record'
+
+module Wlog
+# Add type, id polymorphic fields
+# @author Simon Symeonidis
+# @date Sun Oct  5 22:43:12 EDT 2014
+class FixAttachmentsPolymorphicTable < AtciveRecord::Migration
+
+  def change
+    add_column :attachments, :attachable_id, :integer
+    add_column :attachments, :attachable_type, :text
+  end
+
+end
+end
