25e90083a8095291c768abc1bd91a11af9f281f0
psyomn <lethaljellybean@gmail.com>
2014-10-05 23:03:52 -0400

    migrations/attachments: fix typo

--- lib/wlog/migrations/fix_attachments_polymorphic_table.rb
+++ lib/wlog/migrations/fix_attachments_polymorphic_table.rb
@@ -4,7 +4,7 @@ module Wlog
 # Add type, id polymorphic fields
 # @author Simon Symeonidis
 # @date Sun Oct  5 22:43:12 EDT 2014
-class FixAttachmentsPolymorphicTable < AtciveRecord::Migration
+class FixAttachmentsPolymorphicTable < ActiveRecord::Migration
 
   def change
     add_column :attachments, :attachable_id, :integer
