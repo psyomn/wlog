16a3b1e91ebf36413f1aedd0005b9d90a4a3b417
psyomn <lethaljellybean@gmail.com>
2014-10-07 00:37:47 -0400

    git/ui: add the git setter ui

---
+++ lib/wlog/ui/git_ui.rb
@@ -0,0 +1,39 @@
+require 'wlog/domain/sys_config'
+module Wlog
+# Interface to setup git stuff.
+# @author Simon Symeonidis
+class GitUi
+  
+  def initialize
+    @strmaker = SysConfig.string_decorator
+  end
+
+  def run 
+    cmd = "default" 
+
+    until cmd == "end" do 
+      cmd = Readline.readline("[#{@strmaker.blue(git)}] ")
+      cmd = gets.chomp
+
+      case cmd 
+      when /^set/
+        path = Readline.readline("Path to git repo (eg: project/.git/): ")
+        
+        unless File.directory? path
+          puts @strmaker.red("That doesn't look like a git repo. Nothing done")
+          next
+        end
+ 
+        # Set the git repo in the db (so one git repo per db)
+        KeyValue.put!("git", path)
+
+      when /^unset/
+        KeyValue.put!("git", "")
+
+      end 
+    end
+  end
+
+end
+end
+
