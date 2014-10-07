f9bc783a69e7e435b1785ec14deba5388e6e309c
psyomn <lethaljellybean@gmail.com>
2014-10-06 21:53:58 -0400

    commands/fetch_git_commits: stubs for git log fetching

---
+++ lib/wlog/commands/fetch_git_commits.rb
@@ -0,0 +1,34 @@
+require 'wlog/commands/commandable'
+module Wlog
+# Given two dates, extract the git commits.
+# @author Simon Symeonidis
+class FetchGitCommits < Commandable
+
+  # @param from   date start
+  # @param to     date end
+  # @param repo   location to the git repo
+  # @param author only show logs of that author. If none is given, fetch all
+  #   authors
+  def initialize(from, to, repo, author=nil)
+    @from, @to, @repo, @author = from, to, repo, author
+  end
+
+  def execute
+    result = `#{run_git_cmd}`
+  end
+
+  attr :commits
+
+private
+
+  # git --git-dir <thedir> log --since=... --until=... --author=...
+  def run_git_cmd
+    base = "git --git-dir #{@repo} log "
+    base.concat("--since=\"#{@from}\" ")
+    base.concat("--until=\"#{@to}\" ")
+    base.concat("--author=\"#{@author}\"") if @author
+  base end
+
+end
+end
+
