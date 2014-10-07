26c32905abe04ee732848c1c15e09fd5ce192429
psyomn <lethaljellybean@gmail.com>
2014-10-06 23:32:38 -0400

    git_commit: add git commit object

---
+++ lib/wlog/domain/git_commit.rb
@@ -0,0 +1,11 @@
+module Wlog
+# A git commit message
+# @author Simon Symeonidis
+class GitCommit 
+  def initialize
+    @commit = @author = @shortlog = @message = ""
+  end
+
+  attr_accessor :commit, :author, :shortlog, :message
+end
+end
