d8957e35ebd076770c3b5a0a078b8882e2519bb2
psyomn <lethaljellybean@gmail.com>
2014-10-07 00:05:12 -0400

    git_commit_parser: add git log parser code

---
+++ lib/wlog/tech/git_commit_parser.rb
@@ -0,0 +1,43 @@
+module Wlog
+# Parses git text from a `git log` command invocation
+# @author Simon Symeonidis
+class GitCommitParser
+  # @param log_s is the string obtained from running a `git log` command
+  # @return a list of GitCommit objects
+  def self.parse(log_s)
+    cur = nil 
+    inmessage = false 
+    gitlogs = [] 
+     
+    log_s.lines.each do |line| 
+      case line
+      when /^commit/i
+        inmessage = false 
+        gitlogs.push cur if cur
+        cur = GitLog.new
+        cur.commit = line.split[1].strip
+                            
+      when /^author/i
+        cur.author = line.split[1].strip
+                                   
+      when /^date/i, /^\n$/
+        next
+                                          
+      else 
+        if inmessage
+          cur.message.concat(line)
+          cur.message.strip!
+        else
+          cur.shortlog = line
+          cur.shortlog.strip! 
+          inmessage = true
+        end 
+      end
+    
+    end
+    
+    gitlogs.push cur if cur
+  gitlogs end
+
+end
+end
