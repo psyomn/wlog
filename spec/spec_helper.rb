17b8ea37f7026cce42824484f0bd7e3141c29209
psyomn <lethaljellybean@gmail.com>
2014-10-09 00:45:53 -0400

    spec: add aux methods to helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,29 @@
 require 'coveralls'
+require 'date'
+require 'wlog/domain/invoice'
+require 'wlog/domain/issue'
+require 'wlog/domain/log_entry'
 Coveralls.wear!
+
+$sn = 0
+
+module DomainHelpers
+  def sn
+    old = $sn
+    $sn += 1
+  old end
+ 
+  # Make a log entry but don't store it
+  def make_log_entry
+    le = LogEntry.new(:description => "Desc ##{sn}")
+  le end
+
+  def make_issue
+    is = Issue.new(:description => "Desc #{sn}",
+      :due_date => DateTime.now, 
+      :status => 0, 
+      :timelog => 10, 
+      :long_description => "big desc #{sn}")
+  is end
+end
+
