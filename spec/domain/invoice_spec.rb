c7200e4e82f585da518ab23be7e4dffad9244c99
psyomn <lethaljellybean@gmail.com>
2014-10-09 00:46:50 -0400

    spec: add simple testing for invoice

--- spec/domain/invoice_spec.rb
+++ spec/domain/invoice_spec.rb
@@ -1,15 +1,34 @@
 require_relative '../spec_helper'
 require_relative '../make_db'
-require 'wlog/domain/invoice'
 
 include Wlog
 
 describe Invoice do 
+  include DomainHelpers
 
   db_name = 'default'
+  db_path = standard_db_path(db_name)
+
+  before :all do 
+    make_testing_db(db_name)
+  end
+
+  after :all do 
+    FileUtils.rm db_path
+  end
 
   it 'should fetch all relevant log entries between two dates' do 
-    pending
+    invoice = Invoice.create(:from => (DateTime.now - 5), :to => (DateTime.now + 5))
+    issue = make_issue
+    
+    10.times do
+      le = make_log_entry
+      le.created_at = DateTime.now - 2
+      issue.log_entries << le
+    end
+    issue.save
+
+    expect(invoice.log_entries_within_dates.count).to eq(10)
   end
 
 end
