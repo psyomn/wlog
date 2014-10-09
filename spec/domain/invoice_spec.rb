9ec8e0e72984fe2d1cbb4a0fb98a07178413f7fd
psyomn <lethaljellybean@gmail.com>
2014-10-09 00:14:40 -0400

    spec: add invoice spec

---
+++ spec/domain/invoice_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../spec_helper'
+require_relative '../make_db'
+require 'wlog/domain/invoice'
+
+include Wlog
+
+describe Invoice do 
+
+  db_name = 'default'
+
+  it 'should fetch all relevant log entries between two dates' do 
+    pending
+  end
+
+end
+
