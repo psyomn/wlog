ecb4465ca86f509b1e02955cce5f68cefa621c49
psyomn <lethaljellybean@gmail.com>
2014-10-09 01:32:12 -0400

    commands/delete_attachment: stubs for delete command

---
+++ lib/wlog/commands/delete_attachment.rb
@@ -0,0 +1,17 @@
+require 'wlog/commands/commandable'
+module Wlog
+# Delete an attachment, given its id (in the context of an issue)
+# @author Simon Symeonidis
+class DeleteAttachment < Commandable
+  def initialize(issue, att_id)
+    @issue  = issue
+    @att_id = att_id
+  end
+
+  def execute
+  end
+
+private
+end
+end
+
