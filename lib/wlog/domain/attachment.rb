47e0a1c78d4a87ff6b5b091ed51283724e5f9df0
psyomn <lethaljellybean@gmail.com>
2014-10-12 03:46:51 -0400

    attachment: add zlib compression to data
    This is in hopes to make things a little more performant when you
    attach big files (things like 30-60mb, and numerous of them).
    
    Note: next step it might be better to have a special method to get
    data information ie: decompress things only when needed.

--- lib/wlog/domain/attachment.rb
+++ lib/wlog/domain/attachment.rb
@@ -6,6 +6,8 @@ module Wlog
 # Following the Active Record pattern
 # OO way of handling blobs of data, to be stored in memory or in db.
 class Attachment < ActiveRecord::Base
+  before_save :compress_string
+  after_initialize :uncompress_string
   belongs_to :attachable, polymorphic: true
 
   def to_s
@@ -14,6 +16,16 @@ class Attachment < ActiveRecord::Base
     str.concat("  [").concat(@strmaker.green(self.id)).concat("] ")
     str.concat(@strmaker.red(self.filename)).concat($/)
   str end
+
+private
+
+  def compress_string
+    Zlib::Deflate.deflate self.data
+  end
+
+  def uncompress_string
+    Zlib::Inflate.inflate self.data
+  end
 
 end
 end # module Wlog
