877baa9d0d155df4e1ee6c16024a909670dbbddf
psyomn <lethaljellybean@gmail.com>
2014-10-12 17:37:21 -0400

    spec: re-enable tests for attachments

--- spec/domain/attachment_spec.rb
+++ spec/domain/attachment_spec.rb
@@ -1,59 +1,55 @@
 # We'll be ignoring this for now...
 #
 require_relative '../spec_helper.rb'
-# require_relative '../make_db'
-# require 'wlog/domain/attachment'
-# require 'wlog/domain/log_entry'
-# require 'wlog/domain/issue'
-# 
-# include Wlog
-# 
-# describe Attachment do 
-# 
-#   db_name = './default'
-#   
-#   class FileMock
-#     attr_accessor :filename, :data, :path
-#   end
-# 
-#   def create_mock_file
-#     fm = FileMock.new
-#     fm.filename = "thefile.txt"
-#     fm.data = "This is my text data." 
-#     fm.path = "/home/user/"
-#   fm end
-# 
-#   before(:all) do
-#     make_testing_db(db_name)
-#     @db = DbRegistry.new(db_name) 
-#     @issue = Issue.new(@db)
-#     @log_entry = LogEntry.new(@db)
-# 
-#     @issue.description = "This is my issue"
-#     @issue.insert
-#     @log_entry.issue_id = @issue.id
-#     @log_entry.description = "This is my log_entry"
-#     @log_entry.insert
-#   end
-# 
-#   after(:all) do
-#     FileUtils.rm db_name
-#   end
-# 
-#   # Note: this example uses issue, but this will be valid for whatever other
-#   # type we want to associate to this class. 
-#   it "should insert given polymorphic type" do
-#     file = create_mock_file
-#     attach = Attachment.new(@db, Issue.name, @issue.id)
-#     attach.filename = file.filename
-#     attach.data = file.data
-#     attach.insert
-#     check = Attachment.find(@db, Issue.name, @issue.id)
-#     expect(check).to_not eq(nil)
-#   end
-# 
-#   it "should return nil if something is not found" do
-#     expect(Attachment.find(@db, Issue.name, 123123123)).to eq(nil)
-#   end
-# end 
+require_relative '../make_db'
+require 'wlog/domain/attachment'
+require 'wlog/domain/log_entry'
+require 'wlog/domain/issue'
+require 'zlib'
+
+include Wlog
+
+describe Attachment do 
+ 
+   db_name = 'default'
+   db_path = standard_db_path(db_name)
+   
+   before(:all) do
+     make_testing_db(db_name)
+   end
+ 
+   after(:all) do |test|
+     if test.exception
+       # ensure that db is removed even if some test fails 
+       FileUtils.rm db_name
+     end
+     FileUtils.rm db_name
+   end
+ 
+   it 'should attach a mock file to an issue' do
+     @issue = Issue.create(:description => 'mydesc',
+       :long_description => 'potato')
+     
+     # You're kind of forced to do this because I've hacked a shitty
+     # implementation of compressing - uncompressing strings automatically when
+     # attaching files.
+     @attachment = Attachment.new
+     @attachment.filename = 'filename'
+     @attachment.given_name = 'given name'
+     @attachment.data = 'data stuff'
+
+     @issue.attachments << @attachment
+     @issue.save
+
+     issue = Issue.find(@issue.id)
+     expect(issue.attachments.count).to eq(1)
+     expect(issue.attachments.first.filename).to eq('filename')
+     expect(issue.attachments.first.given_name).to eq('given name')
+     expect(issue.attachments.first.data).to eq('data stuff')
+   end
+
+   it "should return nil if something is not found" do
+     expect(Attachment.find_by_id(123123123)).to eq(nil)
+   end
+end 
 # 
