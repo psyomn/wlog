85a897b72ae68835ee8a533dd61467017cd32c98
psyomn <lethaljellybean@gmail.com>
2014-10-12 04:35:44 -0400

    doc: fetch_git_commits explanation

--- lib/wlog/commands/fetch_git_commits.rb
+++ lib/wlog/commands/fetch_git_commits.rb
@@ -5,11 +5,19 @@ module Wlog
 # @author Simon Symeonidis
 class FetchGitCommits < Commandable
 
+  # Configuration to query the git repo for the required commits. You can
+  # specify a date range and an author. You need to provide a path to the
+  # git repository.
   # @param from   date start
   # @param to     date end
   # @param repo   location to the git repo
   # @param author only show logs of that author. If none is given, fetch all
   #   authors
+  # @example 
+  #   from_date = DateTime.now - 15
+  #   to_date   = DateTime.now + 5
+  #   repo      = '/home/jon/wlog/.git/'
+  #   cmd = FetchGitCommits.new(from_date, to_date, repo, 'jon')
   def initialize(from, to, repo, author=nil)
     @from, @to, @repo, @author = from, to, repo, author
   end
