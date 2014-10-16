ef3da57d46be346a14edf5713417453aad5a3bef
psyomn <lethaljellybean@gmail.com>
2014-10-15 22:43:27 -0400

    setup_wizard: more human friendly way of doing things

--- lib/wlog/ui/setup_wizard.rb
+++ lib/wlog/ui/setup_wizard.rb
@@ -1,6 +1,7 @@
 require 'readline'
 require 'wlog/domain/sys_config'
 require 'wlog/commands/taint_setup'
+require 'wlog/tech/wlog_string'
 
 module Wlog
 # This is the ui that is displayed whenever we detect that it is the first
@@ -21,7 +22,8 @@ class SetupWizard
 private 
 
   def get_setting
-    question = "Do you use a terminal that supports ANSI colors? [yes/no] :"
+    puts WlogString.green('ABCD')
+    question = "Is the previous line a green 'ABCD'? [yes/no] :"
     until ['yes', 'no'].include? input
       @input = Readline.readline(question)
       @input.chomp!
