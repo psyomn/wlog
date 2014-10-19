9e66afbe031fd2bb366e299bf507c2b3fabe962d
Nick Nadboralski <nnadboralski@unite4good.org>
2014-10-19 18:06:03 -0500

    Added help for git

--- lib/wlog/ui/git_ui.rb
+++ lib/wlog/ui/git_ui.rb
@@ -2,46 +2,59 @@ require 'wlog/domain/sys_config'
 module Wlog
 # Interface to setup git stuff.
 # @author Simon Symeonidis
-class GitUi
-  
-  def initialize
-    @strmaker = SysConfig.string_decorator
-  end
+  class GitUi
 
-  def run 
-    cmd = "default" 
+    def initialize
+      @strmaker = SysConfig.string_decorator
+    end
 
-    until cmd == "end" do 
-      cmd = Readline.readline("[#{@strmaker.blue('git')}] ")
+    def run
+      cmd = "default"
 
-      case cmd 
-      when /^set/
-        path = Readline.readline("Path to git repo (eg: project/.git/): ")
-        
-        unless File.directory? path
-          puts @strmaker.red("That doesn't look like a git repo. Nothing done")
-          next
+      until cmd == "end" do
+        cmd = Readline.readline("[#{@strmaker.blue('git')}] ")
+
+        case cmd
+        when /^set/
+          path = Readline.readline("Path to git repo (eg: project/.git/): ")
+
+          unless File.directory? path
+            puts @strmaker.red("That doesn't look like a git repo. Nothing done")
+            next
+          end
+
+          author = Readline.readline("git author: ")
+
+          # Set the git repo in the db (so one git repo per db)
+          KeyValue.put!("git", path)
+          KeyValue.put!("author", author)
+
+        when /^unset/
+          KeyValue.put!("git", "")
+
+        when /^(ls|show)/
+          print '  repo: '
+          puts @strmaker.green(KeyValue.get("git"))
+          print '  auth: '
+          puts @strmaker.yellow(KeyValue.get("author"))
+
+        when /^help/
+          print_help
         end
-
-        author = Readline.readline("git author: ")
- 
-        # Set the git repo in the db (so one git repo per db)
-        KeyValue.put!("git", path)
-        KeyValue.put!("author", author)
-
-      when /^unset/
-        KeyValue.put!("git", "")
-
-      when /^(ls|show)/
-        print '  repo: '
-        puts @strmaker.green(KeyValue.get("git"))
-        print '  auth: '
-        puts @strmaker.yellow(KeyValue.get("author"))
-
       end
     end
-  end
 
-end
+    private
+
+    def print_help
+      ['ls, show', 'list the current git repository settings',
+       'set', 'set repo and author for currnet git repository',
+       'unset', 'unsets git repository settings',
+       'help', 'print this menu'].each_with_index do |cmd,ix|
+         print "  " if ix % 2 == 1
+         puts cmd
+       end
+    end
+  end
 end
 
